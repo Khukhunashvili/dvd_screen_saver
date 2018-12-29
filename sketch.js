afa87cbeb1a5020dcd0f621b89b273bd8a031cb3
FiNkacho <sabaxuxunashvili@gmail.com>
2018-12-29 21:37:43 +0400

    added wall hit counter

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var x, y;
 var speedX = 5;
 var speedY = 5;
 
+var wallHits = 0;
+
 function preload(){
   dvdLogo = loadImage("images/dvd.png");
 }
@@ -13,10 +15,16 @@ function setup() {
   x = windowWidth/2 - dvdLogo.width/4;
   y = windowHeight/2 - dvdLogo.height/4;
 
+  textSize(20);
+
 }
 
 function draw() {
   background(0);
+
+  fill(255);
+  text("Wall hits:", 30, 100);
+  text(wallHits, 130, 100);
 
   image(dvdLogo, x, y, dvdLogo.width/2, dvdLogo.height/2)
 
@@ -25,10 +33,12 @@ function draw() {
 
   if(y+dvdLogo.height/2 > windowHeight || y < 0 ) {
     speedY = -speedY;
+    wallHits += 1;
   }
 
   if(x+dvdLogo.width/2 > windowWidth || x < 0) {
     speedX = -speedX;
+    wallHits += 1;
   }
 
 }
